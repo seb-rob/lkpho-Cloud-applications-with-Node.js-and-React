50163678321d0c4c57be63a402e71c5ae42d8c97
sebastian robi roy <sebastianrobi29@gmail.com>
2023-10-06 04:57:30 -0400

    final commit

--- CD220Labs/expressjs/expressServer.js
+++ CD220Labs/expressjs/expressServer.js
@@ -20,6 +20,20 @@ app.get("/:name",(req,res)=>{
     res.send("Hello "+req.params.name)
 })
 
+// fetch months
+app.get("/fetchMonth/:num", (req, res) => {
+    const months = [
+        "January", "February", "March", 
+        "April", "May", "June", "July", "August", 
+        "September", "October", "November", "December"
+    ]
+    const number = parseInt(req.params.num)
+    if(number < 1 || number > 12){
+        return res.status(400).json({message: "Not a valid month number"})
+    }
+    return res.send(months[number-1])
+})
+
 app.listen(3333, () => {
     console.log(`Listening at http://localhost:3333`)
 })
